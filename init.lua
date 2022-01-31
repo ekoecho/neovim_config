0d1aa851be837c6b5b8db5a11a40a5db7f74ace4
Graham Clark <gclark@getaroom.com>
2022-01-31 21:10:30 +0000

    Updates and a couple of new plugins

--- init.lua
+++ init.lua
@@ -1,17 +1,19 @@
-require "user.options"
-require "user.plugins"
-require "user.colorscheme"
-require "user.keymaps"
-require "user.cmp"
-require "user.crates"
-require "user.lsp"
-require "user.lualine"
-require "user.gitsigns"
-require "user.tree-sitter"
-require "user.alpha"
-require "user.project"
-require "user.nvim-tree"
-require "user.lsp-signature"
-require "user.rust-tools"
+require("user.options")
+require("user.plugins")
+require("user.colorscheme")
+require("user.keymaps")
+require("user.cmp")
+require("user.crates")
+require("user.lsp")
+require("user.lualine")
+require("user.gitsigns")
+require("user.tree-sitter")
+require("user.alpha")
+require("user.project")
+require("user.nvim-tree")
+require("user.lsp-signature")
+require("user.rust-tools")
+require("user.telescope-vimwiki")
+require("user.vimwiki")
 -- require "user.lspsaga"
 --require "ftplugin.rust"
--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -18,20 +18,23 @@ vim.g.maplocalleader = " "
 --   term_mode = "t",
 --   command_mode = "c",
 
-keymap('n', '<Leader><Leader>', ':source $MYVIMRC<CR>', opts)
-
+keymap("n", "<Leader><Leader>", ":source $MYVIMRC<CR>", opts)
 
 -- Telescope
 
-keymap('n', '<C-p>', ':Telescope find_files<CR>', opts)
-keymap('n', '<Leader>b', ':Telescope buffers<CR>', opts)
-keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-keymap('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-keymap('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-keymap('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-keymap('n', '±', ':NvimTreeToggle<CR>', opts)
+keymap("n", "<C-p>", ":Telescope find_files<CR>", opts)
+keymap("n", "<Leader>b", ":Telescope buffers<CR>", opts)
+keymap("n", "<Leader>p", ":Telescope projects<CR>", opts)
+keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+keymap("n", "<leader>af", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+keymap("n", "<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+keymap("n", "±", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<leader>-", ":split<CR>", opts)
+keymap("n", "<leader>|", ":vsplit<CR>", opts)
+keymap("n", "<leader>a", ":Alpha<CR>", opts)
+keymap("n", "<leader>s", ":SymbolsOutline<CR>", opts)
 
-
-vim.cmd 'cmap w!! w !sudo tee % >/dev/null'
+vim.cmd("cmap w!! w !sudo tee % >/dev/null")
