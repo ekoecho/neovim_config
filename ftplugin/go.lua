649c592b8f2fc1e57b843668e9656ca5e634bea9
Graham Clark <geeronimo@gmail.com>
2022-06-18 19:25:14 +0100

    add go quit shortcut

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -8,11 +8,16 @@ function! GoTest()
   call VimuxRunCommand("clear ; cd ".getcwd()."; go test -v")
 endfunction
 
+function! GoQuit()
+  call VimuxSendKeys("C-c")
+endfunction
+
 
 map <Leader>rr :wa<CR> :call GoRun()<CR>
 map <Leader>gr :wa<CR> :call GoRun()<CR>
 map <Leader>rt :wa<CR> :call GoTest()<CR>
 map <Leader>gt :wa<CR> :call GoTest()<CR>
+map <Leader>gx :wa<CR> :call GoQuit()<CR>
 map <f5> :wa<CR> :call GoRun()<CR>
 map <f6> :wa<CR> :call GoTest()<CR>
 ]])
